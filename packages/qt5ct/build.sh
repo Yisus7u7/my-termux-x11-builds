e47de1f2eaefbd61bdd5b7feb668763c3f7e1391
Yisus7u7 <jesuspixel5@gmail.com>
2021-08-24 11:00:59 -0500

    add package: qt5ct

---
+++ packages/qt5ct/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/desktop-app/qt5ct
+TERMUX_PKG_DESCRIPTION="This program allows users to configure Qt5 settings (theme, font, icons, etc.) under DE/WM without Qt integration"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
+TERMUX_PKG_VERSION=1.1
+TERMUX_PKG_SRCURL=https://github.com/desktop-app/qt5ct/archive/refs/heads/master.zip
+TERMUX_PKG_SHA256=59c8889189808cfcd42cf49bea42589c82a45e462029bfc7c17824d955f75db1
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, libx11"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+
