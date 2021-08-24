c6ed2c066802efc8b136d545b6e6124494e83b63
Yisus7u7 <jesuspixel5@gmail.com>
2021-08-24 11:37:28 -0500

    fix deps for qt5ct

--- packages/qt5ct/build.sh
+++ packages/qt5ct/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=1.1
 TERMUX_PKG_SRCURL=https://github.com/desktop-app/qt5ct/archive/refs/heads/master.zip
 TERMUX_PKG_SHA256=59c8889189808cfcd42cf49bea42589c82a45e462029bfc7c17824d955f75db1
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, libx11"
-TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools, qt5-qttools"
 
 termux_step_pre_configure(){
 	mkdir -p ${TERMUX_PREFIX}/libexec/qt/platformthemes
