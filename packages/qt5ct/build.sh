0083e6983b1688eaf31a62d63bc710814d2c0847
Yisus7u7 <jesuspixel5@gmail.com>
2021-08-24 11:49:16 -0500

    move qt5-qttools for TERMUX_PKG_DEPENDS

--- packages/qt5ct/build.sh
+++ packages/qt5ct/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=1.1
 TERMUX_PKG_SRCURL=https://github.com/desktop-app/qt5ct/archive/refs/heads/master.zip
 TERMUX_PKG_SHA256=59c8889189808cfcd42cf49bea42589c82a45e462029bfc7c17824d955f75db1
-TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, libx11"
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, libx11, qt5-qttools"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools, qt5-qttools"
 
 termux_step_pre_configure(){
