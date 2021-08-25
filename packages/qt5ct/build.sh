0e60548bf9af6cbcc86fca8c486e49b5388f74bb
Yisus7u7 <jesuspixel5@gmail.com>
2021-08-25 09:17:08 -0500

    use qmake for cross tools

--- packages/qt5ct/build.sh
+++ packages/qt5ct/build.sh
@@ -5,8 +5,8 @@ TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=1.1
 TERMUX_PKG_SRCURL=https://github.com/desktop-app/qt5ct/archive/refs/heads/master.zip
 TERMUX_PKG_SHA256=59c8889189808cfcd42cf49bea42589c82a45e462029bfc7c17824d955f75db1
-TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, libx11, qt5-qttools"
-TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools, qt5-qttools"
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, libx11"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 
 termux_step_pre_configure(){
 	mkdir -p ${TERMUX_PREFIX}/libexec/qt/platformthemes
@@ -18,7 +18,7 @@ termux_step_pre_configure(){
 	
 termux_step_configure(){
 	cd ${TERMUX_PKG_SRCDIR}
-	qmake PREFIX=${TERMUX_PREFIX} .
+	"${TERMUX_PREFIX}/opt/qt/cross/bin/qmake" .
 	}
 	
 termux_step_make(){
